9b5caa02f5d33eff8aa85ceefbc311b733759adf
adalovlace <105163654+adalovlace@users.noreply.github.com>
2022-06-16 01:11:59 +0300

    Add files via upload

---
+++ teksailaritoplama.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class teksailaritoplama {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n;
+        int toplam = 0;
+
+        do {
+            System.out.println("Lütfen bir sayı giriniz ?");
+            n = input.nextInt();
+            if (n % 2 == 1) { 
+
+                toplam += n;
+            }
+
+
+        } while (n > 0);
+        System.out.println("Tek sayılar toplamı : " + toplam);
+    }
+}
